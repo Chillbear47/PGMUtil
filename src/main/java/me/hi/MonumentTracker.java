ff73d016351250093a43ed7858b197aa3a190598
BuildTools <unconfigured@null.spigotmc.org>
2025-06-06 00:10:15 +0200

    PGM util

--- src/main/java/me/hi/MonumentTracker.java
+++ src/main/java/me/hi/MonumentTracker.java
@@ -1,21 +1,26 @@
 package me.hi;
 
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import tc.oc.pgm.api.match.Match;
-import tc.oc.pgm.api.player.MatchPlayer;
-import tc.oc.pgm.destroyable.DestroyableMatchModule;
-import tc.oc.pgm.destroyable.Destroyable;
-import tc.oc.pgm.teams.Team;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.util.Vector;
+import tc.oc.pgm.api.match.Match;
+import tc.oc.pgm.api.player.MatchPlayer;
+import tc.oc.pgm.destroyable.Destroyable;
+import tc.oc.pgm.destroyable.DestroyableMatchModule;
+import tc.oc.pgm.teams.Team;
+import tc.oc.pgm.regions.FiniteBlockRegion;
+import com.sk89q.worldedit.math.BlockVector3;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MonumentTracker implements Listener {
+
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -42,6 +47,7 @@ public class MonumentTracker implements Listener {
         if (dtm == null) return;
 
         Team playerTeam = (Team) player.getParty();
+        // Correct observer check: use isObserving() from Party interface, implemented by Team
         if (playerTeam == null || playerTeam.isObserving()) return;
 
         // Get all enemy monuments not yet destroyed
@@ -55,11 +61,21 @@ public class MonumentTracker implements Listener {
         // Find the closest monument
         Location playerLoc = player.getBukkit().getLocation();
         Destroyable closest = enemyMonuments.stream()
-                .min(Comparator.comparingDouble(obj -> obj.getBlockRegion().getCenter().distance(playerLoc)))
+                .min(Comparator.comparingDouble(obj -> getRegionCenter(obj.getBlockRegion(), playerLoc.getWorld()).distance(playerLoc)))
                 .orElse(null);
 
         if (closest != null) {
-            player.getBukkit().setCompassTarget(closest.getBlockRegion().getCenter());
+            player.getBukkit().setCompassTarget(getRegionCenter(closest.getBlockRegion(), playerLoc.getWorld()));
         }
     }
-}
+
+    // Utility to calculate the center Location of a FiniteBlockRegion
+    private Location getRegionCenter(FiniteBlockRegion region, World world) {
+        Vector min = region.getBounds().getMin();
+        Vector max = region.getBounds().getMax();
+        double centerX = (min.getBlockX() + max.getBlockX()) / 2.0 + 0.5;
+        double centerY = (min.getBlockY() + max.getBlockY()) / 2.0 + 0.5;
+        double centerZ = (min.getBlockZ() + max.getBlockZ()) / 2.0 + 0.5;
+        return new Location(world, centerX, centerY, centerZ);
+    }
+    }
